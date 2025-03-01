12b775c9f7e4649744f440e646d3863c116513df
Artem Guzhva <21160542+hexcat@users.noreply.github.com>
2025-03-01 18:27:36 +0000

    Add hexcat-kali plugin

---
+++ dot_oh-my-zsh/custom/plugins/hexcat-kali/hexcat-kali.plugin.zsh
@@ -0,0 +1,8 @@
+: ${HEXCAT_USER_AGENT:="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"}
+
+alias nst="sudo nmap -v -T4 -Pn -A"
+alias nsa="sudo nmap -v -T4 -Pn -A -p-"
+alias nsaf="sudo nmap -v -T4 -Pn -A -p- --defeat-rst-ratelimit"
+alias nu="sudo nmap -v -T4 -Pn -sU -sV --version-intensity=0"
+
+alias ffuf="ffuf -H '${HEXCAT_USER_AGENT}'"
