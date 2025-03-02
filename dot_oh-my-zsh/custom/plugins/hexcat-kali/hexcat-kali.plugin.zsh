a8f924c75347e45346dbd75ed37d93c34c656a09
Artem Guzhva <21160542+hexcat@users.noreply.github.com>
2025-03-02 18:17:22 +0000

    Echo commands with expanded variables

--- dot_oh-my-zsh/custom/plugins/hexcat-kali/hexcat-kali.plugin.zsh
+++ dot_oh-my-zsh/custom/plugins/hexcat-kali/hexcat-kali.plugin.zsh
@@ -5,4 +5,14 @@ alias nsa="sudo nmap -v -T4 -Pn -A -p-"
 alias nsaf="sudo nmap -v -T4 -Pn -A -p- --defeat-rst-ratelimit"
 alias nu="sudo nmap -v -T4 -Pn -sU -sV --version-intensity=0"
 
-alias ffuf="ffuf -H '${HEXCAT_USER_AGENT}'"
+alias ffuf='ffuf -H "${HEXCAT_USER_AGENT}"'
+
+custom-accept-line() {
+  EBUFFER=${(e)BUFFER}
+  if [[ "$EBUFFER" != "$BUFFER" ]]; then
+    echo
+    echo -n ">> ${(e)BUFFER}"
+  fi
+  zle .accept-line
+}
+zle -N accept-line custom-accept-line
